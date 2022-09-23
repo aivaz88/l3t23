b9a715040f5833c5a76585ceaef9e0f6bdfdd2e2
aivaz88 <aivaz1988@mail.ru>
2022-09-23 14:00:11 +0500

    comment

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 0;
+
+while(count <= num)
+{
+    count++;
+    Console.Write(Math.Pow(count, 3));
+    if (count <= num)
+        Console.Write(", ");
+}
